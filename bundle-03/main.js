eb81367f4895d88f611d1b166501a8a956925272
Giadaspt <saputo.giada@gmail.com>
2021-12-09 18:54:49 +0100

    completato numero 3

--- bundle-03/main.js
+++ bundle-03/main.js
@@ -56,7 +56,7 @@ const cars = [
         manufacturer: 'Seat',
         model: 'Ibiza',
         type: 'metano'
-    }
+    },
     {
         manufacturer: 'Audi',
         model: 'R8',
@@ -64,14 +64,14 @@ const cars = [
     },
 ];
 
-const gasolineCars = cars.filter( (auto) >= auto.type === 'benzina');
+const gasolineCars = cars.filter( (auto) => auto.type.toLowerCase() === 'benzina'); 
 
 const dieselCars = cars.filter( (auto) => {
-    auto.type === 'diesel';
+   return auto.type.toLowerCase() === 'diesel'; 
 });
 
 const otherCars = cars.filter( (auto) => {
-    return auto.type !== 'benzina' || auto.type !== 'diesel';
+    return auto.type.toLowerCase() !== 'benzina' && auto.type.toLowerCase() !== 'diesel'; 
 });
 
 console.log('Auto a benzina');
@@ -84,3 +84,7 @@ console.log(dieselCars);
 
 console.log('Tutte le altre auto');
 console.log(otherCars);
+
+// 1. questo codice serve per filtrare le maccchine dividendole per tipologia di carburante: quelle a benzina, quelle diesel e poi tutte le altre (metano, gpl etc...).
+// 2. errori di sintassi: 1) riga 59 mancava una virgola dopo la chiusura dell'oggetto, 2) nella funzione filter a riga 67 la freccia era al contrario quindi invece di essere => era messa >= che significa maggiore-uguale a ...
+// 3. errori di logica: 1) tutte le volte in cui si ricerca un valore di una proprietà o in generale una stringa si deve sempre aggiungere LowerCase o UpperCase perchè altrimenti potrebbero le maiuscole o minuscole non vengono calcolate e quindi la validazione della stringa è sbagliata questo errore è stato riscontrato in tutte e tre le funzioni filter, 2) mancava il return a riga 70 (filter prevede sempre il return, con foreach e map non è indispensabile), 3) a riga 74 invece di or (||)c'andava and (&&) perchè vanno esclusi entrambi i carburanti per avere i restanti.
